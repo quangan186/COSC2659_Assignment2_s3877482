e7f909c2967ed253756d6c393a057b274530095d
An Bui <quangan186@gmail.com>
2022-08-17 20:56:56 +0700

    setup

---
+++ Tic tac toe/Tic tac toe/Tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_tac_toeApp.swift
+//  Tic tac toe
+//
+//  Created by Bui Quang An on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
